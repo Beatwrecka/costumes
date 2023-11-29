8cc4ec3239c056ca143187e5a05960fba6e69bac
jonny dunseath <beatwrecka@hotmail.co.uk>
2023-11-29 15:24:34 +0000

    added descriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,16 +13,10 @@ Costume.destroy_all
 
 puts "Creating db"
 
-Costume.create(image_url: "red.png", name: "Darth Vader", description: "Historical costume reproduction from the 18th century, handmade in taffeta and enriched with trimmings and lace
-  Comprehends Jacket, waistcoat and trousers", price: 200 )
-Costume.create(image_url: "pink.png", name: "Ballerina ", description: "Historical costume reproduction from the 18th century, handmade in taffeta and enriched with trimmings and lace
-  Comprehends Jacket, waistcoat and trousers", price: 300)
-Costume.create(image_url: "kid.png", name: "Mr, T Costume", description: "Historical costume reproduction from the 18th century, handmade in taffeta and enriched with trimmings and lace
-  Comprehends Jacket, waistcoat and trousers", price: 250)
-Costume.create(image_url: "dress.png", name: "Historical Costume of the 1800s", description: "Historical costume reproduction from the 18th century, handmade in taffeta and enriched with trimmings and lace
-  Comprehends Jacket, waistcoat and trousers", price: 200)
-Costume.create(image_url: "donald.png", name: "Donald Trump Costume", description: "Historical costume reproduction from the 18th century, handmade in taffeta and enriched with trimmings and lace
-  Comprehends Jacket, waistcoat and trousers", price: 150)
-Costume.create(image_url: "centa.png", name: "Santa Claus", description: "Historical costume reproduction from the 18th century, handmade in taffeta and enriched with trimmings and lace
-    Comprehends Jacket, waistcoat and trousers", price: 150)
+Costume.create(image_url: "red.png", name: "Darth Vader", description: "A beautiful pink suit worn by the Dark Lord on his downtime", price: 200 )
+Costume.create(image_url: "pink.png", name: "Ballerina ", description: "Everyone can look pretty in pink with this Ballerina's outfit sported here by Heavyweight Boxing Legend Iron Mike Tyson!", price: 300)
+Costume.create(image_url: "kid.png", name: "Mr, T Costume", description: "Love him or hate him here's big Don wearing a classic Gentleman's outfit", price: 250)
+Costume.create(image_url: "dress.png", name: "Historical Costume of the 1800s", description: "He doens't drink milk or get on no planes, but here's a MR T costume for your little one.", price: 200)
+Costume.create(image_url: "donald.png", name: "Donald Trump Costume", description: "A classic look straight from Mary Antoinette's wardrobe.", price: 150)
+Costume.create(image_url: "centa.png", name: "Santa Claus", description: "Lil Wayne's got Ho Ho Ho's in this seasonal Santa Suit!", price: 150)
 puts "done"
