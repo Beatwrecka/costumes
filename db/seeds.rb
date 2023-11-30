89322afad21d1b7fbb1ffd77fdcc8815a43a27dc
kiran aziz <kiranaziz637@gmail.com>
2023-11-30 15:00:35 +0000

    addedbookingform

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 puts "Cleaning db..."
+Booking.destroy_all
 Costume.destroy_all
 
 puts "Creating db"
@@ -14,17 +15,17 @@ costume2.photos.attach(io: file2, filename: "pink.png", content_type: "image/png
 costume2.save
 
 file3 = File.open(File.join(__dir__, "../app/assets/images/kid.png"))
-costume3 = Costume.create( name: "Mr, T Costume", description: "Love him or hate him here's big Don wearing a classic Gentleman's outfit", price: 250)
+costume3 = Costume.create( name: "Mr, T Costume", description: "He doens't drink milk or get on no planes, but here's a MR T costume for your little one.", price: 250)
 costume3.photos.attach(io: file3, filename: "kid.png", content_type: "image/png")
 costume3.save
 
 file4 = File.open(File.join(__dir__, "../app/assets/images/dress.png"))
-costume4 = Costume.create(name: "Historical Costume of the 1800s", description: "He doens't drink milk or get on no planes, but here's a MR T costume for your little one.", price: 200 )
+costume4 = Costume.create(name: "Historical Costume of the 1800s", description: "A classic look straight from Mary Antoinette's wardrobe.", price: 200 )
 costume4.photos.attach(io: file4, filename: "dress.png", content_type: "image/png")
 costume4.save
 
 file5 = File.open(File.join(__dir__, "../app/assets/images/donald.png"))
-costume5 = Costume.create(name: "Donald Trump Costume", description: "A classic look straight from Mary Antoinette's wardrobe.", price: 150)
+costume5 = Costume.create(name: "Donald Trump Costume", description: "Love him or hate him here's big Don wearing a classic Gentleman's outfit", price: 150)
 costume5.photos.attach(io: file5, filename: "donald.png", content_type: "image/png")
 costume5.save
 
